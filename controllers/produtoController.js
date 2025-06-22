0129982dc0dc4f91d410b370d42d49af40fd2021
guilherme <guilhermesantosandrade01@gmail.com>
2025-06-22 11:25:09 -0300

    tratamento de erros

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -2,45 +2,80 @@ const Produto = require('../models/Produto');
 
 // Listar todos
 exports.getAllProdutos = async (req, res) => {
-  const produtos = await Produto.find();
-  res.json(produtos);
+  try {
+    const produtos = await Produto.find();
+    res.json(produtos);
+  } catch (error) {
+    console.error('Erro ao listar produtos:', error);
+    res.status(500).json({ message: 'Erro interno ao listar produtos' });
+  }
 };
 
 // Listar por ID ou Nome
 exports.getProduto = async (req, res) => {
-  const { id, nome } = req.query;
-  let produto;
+  try {
+    const { id, nome } = req.query;
+    let produto;
 
-  if (id) {
-    produto = await Produto.findById(id);
-  } else if (nome) {
-    produto = await Produto.findOne({ nome });
-  }
+    if (id) {
+      produto = await Produto.findById(id);
+    } else if (nome) {
+      produto = await Produto.findOne({ nome });
+    } else {
+      return res.status(400).json({ message: 'ID ou nome são necessários' });
+    }
 
-  if (produto) {
-    res.json(produto);
-  } else {
-    res.status(404).json({ message: 'Produto não encontrado' });
+    if (produto) {
+      res.json(produto);
+    } else {
+      res.status(404).json({ message: 'Produto não encontrado' });
+    }
+  } catch (error) {
+    console.error('Erro ao buscar produto:', error);
+    res.status(500).json({ message: 'Erro interno ao buscar produto' });
   }
 };
 
 // Cadastrar
 exports.createProduto = async (req, res) => {
-  const produto = new Produto(req.body);
-  await produto.save();
-  res.status(201).json(produto);
+  try {
+    const produto = new Produto(req.body);
+    await produto.save();
+    res.status(201).json(produto);
+  } catch (error) {
+    console.error('Erro ao criar produto:', error);
+    res.status(500).json({ message: 'Erro interno ao criar produto' });
+  }
 };
 
 // Atualizar
 exports.updateProduto = async (req, res) => {
-  const { id } = req.params;
-  const produto = await Produto.findByIdAndUpdate(id, req.body, { new: true });
-  res.json(produto);
+  try {
+    const { id } = req.params;
+    const produto = await Produto.findByIdAndUpdate(id, req.body, { new: true });
+    if (produto) {
+      res.json(produto);
+    } else {
+      res.status(404).json({ message: 'Produto não encontrado' });
+    }
+  } catch (error) {
+    console.error('Erro ao atualizar produto:', error);
+    res.status(500).json({ message: 'Erro interno ao atualizar produto' });
+  }
 };
 
 // Deletar
 exports.deleteProduto = async (req, res) => {
-  const { id } = req.params;
-  await Produto.findByIdAndDelete(id);
-  res.json({ message: 'Produto deletado com sucesso' });
+  try {
+    const { id } = req.params;
+    const produto = await Produto.findByIdAndDelete(id);
+    if (produto) {
+      res.json({ message: 'Produto deletado com sucesso' });
+    } else {
+      res.status(404).json({ message: 'Produto não encontrado' });
+    }
+  } catch (error) {
+    console.error('Erro ao deletar produto:', error);
+    res.status(500).json({ message: 'Erro interno ao deletar produto' });
+  }
 };
